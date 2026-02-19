f980bf14c1463f0e1c71185ef36f257f4b724f59
Indrajit Dey <ijitde1892@gmail.com>
2026-02-19 15:33:11 +0530

    feat: add vercel.json and update vite config for cross-platform deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/notely-notes-app/',
+  // base: '/notely-notes-app/',
+  base: process.env.VERCEL ? '/' : '/notely-notes-app/',
 })
