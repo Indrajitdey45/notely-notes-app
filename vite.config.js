fecc7cc2e87a61a3dde57094dcce6b9f0edd2ab0
Indrajit Dey <ijitde1892@gmail.com>
2026-05-15 20:14:15 +0530

    Responsive for mobile

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/notely-notes-app/',
-  base: process.env.VERCEL ? '/' : '/notely-notes-app/',
+  base: '/',
 })
